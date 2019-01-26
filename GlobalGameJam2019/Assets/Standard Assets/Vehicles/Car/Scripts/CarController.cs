762760d9db1bd9caf2451bd16bc774f0d2d358d2
John Comrie <John_comrie@hotmail.co.uk>
2019-01-26 14:33:35 +0000

    Revert "FMOD RIPPED OUT OF PROJECT"
    This reverts commit 5cfa419a223f5a2501748c4b03c74f785347407b.

--- GlobalGameJam2019/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ GlobalGameJam2019/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -55,6 +55,15 @@ namespace UnityStandardAssets.Vehicles.Car
         public float Revs { get; private set; }
         public float AccelInput { get; private set; }
 
+        // FMOD
+
+        [FMODUnity.EventRef]
+        public string PoloEngineEvent = "event:/Polo/PoloEngine";
+        FMOD.Studio.EventInstance PoloEngine;
+
+        [FMODUnity.EventRef]
+        public string PoloSkidEvent = "event:/Polo/PoloSkid";
+        FMOD.Studio.EventInstance PoloSkid;
 
         // Use this for initialization
         private void Start()
@@ -71,8 +80,19 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Rigidbody = GetComponent<Rigidbody>();
             m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl*m_FullTorqueOverAllWheels);
 
+            PoloEngine.release();
+            PoloEngine = FMODUnity.RuntimeManager.CreateInstance(PoloEngineEvent);
+            //Until Health Bar
+            PoloEngine.setParameterValue("Health", 1);
+            PoloEngine.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+
+            PoloEngine.start();
         }
 
+        private void Update()
+        {
+            PoloEngine.setParameterValue("Speed", CurrentSpeed);
+        }
 
         private void GearChanging()
         {
@@ -283,7 +303,7 @@ namespace UnityStandardAssets.Vehicles.Car
                 {
                     m_WheelEffects[i].EmitTyreSmoke();
 
- 
+                    PoloSkid = FMODUnity.RuntimeManager.CreateInstance(PoloSkidEvent);
 
                     // avoiding all four tires screeching at the same time
                     // if they do it can lead to some strange audio artefacts
@@ -302,7 +322,7 @@ namespace UnityStandardAssets.Vehicles.Car
                 // end the trail generation
                 m_WheelEffects[i].EndSkidTrail();
 
-
+                PoloSkid.release();
             }
         }
 
