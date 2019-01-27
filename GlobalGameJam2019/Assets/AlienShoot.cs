6181d3dedfa5279762a3f1598cff17ba225b46b9
John Comrie <John_comrie@hotmail.co.uk>
2019-01-27 12:50:16 +0000

    Created UFO

---
+++ GlobalGameJam2019/Assets/AlienShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienShoot : MonoBehaviour {
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootInterval = 3.0f;
+    float nextShootTime;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Time.time > nextShootTime)
+        {
+            nextShootTime = Time.time + shootInterval;
+            GameObject b = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            b.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, -1000.0f, 0.0f));
+
+        }
+	}
+}
--- GlobalGameJam2019/Assets/BulletScript.cs
+++ GlobalGameJam2019/Assets/BulletScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
 
+    [SerializeField] GameObject ExplosionParticles;
+    public float power = 100.0f;
+    public float radius = 10.0f;
+    public float upForce = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,27 @@ public class BulletScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(transform.forward);
+        
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+        Instantiate(ExplosionParticles, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Vector3 explosionPosition = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach(Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPosition, radius, upForce, ForceMode.Impulse);
+            }
+        }
+    }
 }
